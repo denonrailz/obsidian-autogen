37c9dc463e73fc1a684a8cb42de8ba1876a08e44
Pavel <ping.06@mail.ru>
2023-12-21 16:48:30 +0300

    Json write to result

--- new_run.py
+++ new_run.py
@@ -1,5 +1,5 @@
 from utils import (
-    read_jsonl, create_timestamp_folder, get_response, collect_data_for_json_result, save_jsonl
+    read_jsonl, create_timestamp_folder, get_response
 )
 from preferences.settings import results_folder, logs_folder, config_list
 
@@ -11,9 +11,16 @@ def main(fake_mode, config) -> None:
     question_list = read_jsonl()
     create_timestamp_folder(results_folder)
     create_timestamp_folder(logs_folder)
-    results = get_response(fake_mode, config, question_list)
-    json_results = collect_data_for_json_result(results)
-    save_jsonl(json_results)
+    get_response(fake_mode, config, question_list)
+    # print(get_response(config, question_list))
+
+
+
+
+
+def save_logs():
+    # сохранить в json фейковый ответ
+    pass
 
 
 def eval():
